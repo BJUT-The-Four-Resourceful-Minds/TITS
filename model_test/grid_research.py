2e67834af59ecabfd33cf60013d55996da190708
duzhe0 <147134207+duzhe0@users.noreply.github.com>
2025-02-24 20:18:47 +0800

    补充注释 (#5)

--- model_test/grid_research.py
+++ model_test/grid_research.py
@@ -4,30 +4,30 @@ from torch import nn
 from module.LSTM import LSTMAutoencoder
 from torch.utils.data import random_split, DataLoader
 from data_processing.timeseries_dataset import TimeSeriesDataset
-from CustomClass import SimpleConcatDataset, SimpleSubset, CustomDataset
+from model_test.CustomClass import SimpleConcatDataset, SimpleSubset, CustomDataset
 from sklearn.metrics import confusion_matrix, accuracy_score, f1_score
 
 
-def AUC(y_true, y_pred):
+def AUC(y_true, y_pred):#计算AUC指标 输入真指标与预测指标两个列表 指标的集合含义是距离左上角的距离
     tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
     tpr = tp / (tp + fn)
     fpr = fp / (fp + tn)
     return np.sqrt((1 - tpr) ** 2 + fpr ** 2)
 
 
-def g_mean(y_true, y_pred):
+def g_mean(y_true, y_pred):#计算G_Mean指标 输入真指标与预测指标两个列表
     tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
     tpr = tp / (tp + fn)
     fpr = fp / (fp + tn)
     return np.sqrt(tpr * (1 - fpr))
 
 
-def load_datasets(window_size):
-    normal_run_path = r'.\Car-Hacking Dataset\normal_run_data\normal_run_data.txt'
-    DoS_dataset_path = r'.\Car-Hacking Dataset\DoS_dataset.csv'
-    Fuzzy_dataset_path = r'.\Car-Hacking Dataset\Fuzzy_dataset.csv'
-    RPM_dataset_path = r'.\Car-Hacking Dataset\RPM_dataset.csv'
-    gear_dataset_path = r'.\Car-Hacking Dataset\gear_dataset.csv'
+def load_datasets(window_size):#加载数据文件
+    normal_run_path = r'../Car-Hacking Dataset/normal_run_data/normal_run_data.txt'
+    DoS_dataset_path = r'../Car-Hacking Dataset/DoS_dataset.csv'
+    Fuzzy_dataset_path = r'../Car-Hacking Dataset/Fuzzy_dataset.csv'
+    RPM_dataset_path = r'../Car-Hacking Dataset/RPM_dataset.csv'
+    gear_dataset_path = r'../Car-Hacking Dataset/gear_dataset.csv'
 
     normal_run_dataset = TimeSeriesDataset(normal_run_path, window_size)
     DoS_dataset = TimeSeriesDataset(DoS_dataset_path, window_size)
@@ -48,7 +48,7 @@ def load_datasets(window_size):
     return train_dataset, test_dataset
 
 
-def prepare_data_loaders(train_dataset):
+def prepare_data_loaders(train_dataset):#返回（X,Label)的数据集
     train_loss_loader = DataLoader(train_dataset)
     X = []
     Label = []
@@ -60,7 +60,7 @@ def prepare_data_loaders(train_dataset):
     return train_loss_loader, train_label_loader, Label
 
 
-def test_model(model, loss_dataloader, criterion, device):
+def test_model(model, loss_dataloader, criterion, device):#遍历训练集生成重建偏差的列表
     model.to(device)
     loss_list = []
     for X, X_next in loss_dataloader:
@@ -70,7 +70,7 @@ def test_model(model, loss_dataloader, criterion, device):
     return loss_list
 
 
-def generate_predictions(Loss, params_grid):
+def generate_predictions(Loss, params_grid):#Label_hat_np的形状是（阈值个数，每个阈值判断条件下的判断结果的个数）
     Label_hat = []
     for threshold in params_grid['threshold']:
         row = []
@@ -85,7 +85,7 @@ def generate_predictions(Loss, params_grid):
     return Label_hat_np
 
 
-def evaluate_metrics(Label, Label_hat, params_grid):
+def evaluate_metrics(Label, Label_hat, params_grid):#不同阈值中寻找，不同指标分别达到最大时阈值的值
     best_accuracy = 0
     best_accuracy_threshold = 0
     best_f1 = 0
@@ -114,7 +114,7 @@ def evaluate_metrics(Label, Label_hat, params_grid):
     print("G - Mean 最大值: {:.4f}, 对应的阈值: {:.4f}".format(best_gmean, best_gmean_threshold))
 
 
-def calculate_AUC_distance(Label, Label_hat, params_grid):
+def calculate_AUC_distance(Label, Label_hat, params_grid):#不同阈值中寻找，AUC指标达到最大时阈值的值
     AUC_distance = []
     for i in range(len(params_grid['threshold'])):
         AUC_distance.append(AUC(Label, Label_hat[i]))
@@ -134,17 +134,19 @@ def main():
     num_layers = 2
     window_size = 10
 
+    #候选的阈值
     params_grid = {'threshold': np.arange(0.05, 0.96, 0.01)}
 
     # 加载数据集
     train_dataset, test_dataset = load_datasets(window_size)
 
     # 准备数据加载器
+    #这里都没有打乱顺序，所以一个索引对应的数据是关联的
     train_loss_loader, train_label_loader, Label = prepare_data_loaders(train_dataset)
 
-    # 初始化分类器
+    # 加载训练好的模型
     model = LSTMAutoencoder(input_size, hidden_size, num_layers)
-    model.load_state_dict(torch.load('model.pt'))
+    model.load_state_dict(torch.load('../model.pt'))
 
     # 测试模型并获取损失列表
     Loss = test_model(model, train_loss_loader, criterion, device)
